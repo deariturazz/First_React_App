d3a4d9360e48faf76b463be21ae5f6d6cf4162a9
Rituraj <ritz.2021@gmail.com>
2025-06-14 20:51:39 +0530

    Added  CSS Module

--- Projects/4_LearningProps/src/components/Item.jsx
+++ Projects/4_LearningProps/src/components/Item.jsx
@@ -1,4 +1,11 @@
+import styles from "./Items.module.css";
+
 const Item = (props) => {
-  return <li className="list-group-item">{props.foodItem}</li>;
+  console.log(styles);
+  return (
+    <li className={`${styles.item} list-group-item`}>
+      <span className={styles.span}>{props.foodItem}</span>
+    </li>
+  );
 };
 export default Item;
