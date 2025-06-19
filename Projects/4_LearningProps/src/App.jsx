b4e2e6f721eaa87118df8b7df1d4ee7498a71501
Rituraj <ritz.2021@gmail.com>
2025-06-19 20:24:53 +0530

    Learning passing children

--- Projects/4_LearningProps/src/App.jsx
+++ Projects/4_LearningProps/src/App.jsx
@@ -2,15 +2,16 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   let foodItems = ["Apple", "Banana", "Carrot", "Doughnut", "Eggplant"];
   return (
-    <>
+    <Container>
       <h1>Healthy Foods</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
-    </>
+    </Container>
   );
 }
 
